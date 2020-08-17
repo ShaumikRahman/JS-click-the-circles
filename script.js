1fad7770dcb56e64db8f393c00f6a6a5abc379f9
Shaumik <shaumikrahman123@gmail.com>
2020-08-17 23:02:47 +0100

    added spawnzone, random postioning for circles

--- script.js
+++ script.js
@@ -5,42 +5,65 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight - document.getElementById('nav').clientHeight;
 
 const size = 33;
+const spawnzoneLeft = size * 2;
+const spawnzoneRight = canvas.width - size * 2;
+const spawnzoneTop = size * 2;
+const spawnzoneBottom = canvas.height - size * 2;
 
 let mousePosX;
 let mousePosY;
 let collisionCheckX;
 let collisionCheckY;
 let distance;
+let numberOfCircles = 3;
+let circleList = [];
+
+function getRand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+for (let i = 0; i < numberOfCircles; i++) {
+    circleList[i] = {
+        x: getRand(spawnzoneLeft, spawnzoneRight),
+        y: getRand(spawnzoneTop, spawnzoneBottom)
+    }
+}
 
 function drawCircle(xPos, yPos) {
     ctx.beginPath();
-    ctx.arc(xPos,yPos,size,0,360);
-    ctx.moveTo(0,0);
+    ctx.arc(xPos, yPos, size, 0, 360);
+    ctx.moveTo(0, 0);
     ctx.closePath();
     ctx.fillStyle = 'white';
     ctx.fill();
 }
 
-drawCircle(200,300);
-
 function update() {
+    for (let i = 0; i < numberOfCircles; i++) {
+        drawCircle(circleList[i].x, circleList[i].y)
 
-    onmousemove = function (e) {
-        mousePosX = e.clientX;
-        mousePosY = e.clientY - document.getElementById('nav').clientHeight;
+        onmousemove = function (e) {
+            mousePosX = e.clientX;
+            mousePosY = e.clientY - document.getElementById('nav').clientHeight;
+        }
+
+        collisionCheckX = circleList[i].x - mousePosX;
+        collisionCheckY = circleList[i].y - mousePosY;
+        distance = Math.sqrt(collisionCheckX * collisionCheckX + collisionCheckY * collisionCheckY);
+
+        if (mousePosX < spawnzoneLeft || 
+            mousePosX > spawnzoneRight || 
+            mousePosY < spawnzoneTop || 
+            mousePosY > spawnzoneBottom) {
+            // out of spawn zone
+        }
+
+        //console.log(`${mousePosX} and ${mousePosY}`);
+
     }
-
-    collisionCheckX = 200 - mousePosX;
-    collisionCheckY = 300 - mousePosY;
-    distance = Math.sqrt(collisionCheckX * collisionCheckX + collisionCheckY * collisionCheckY);
-
-    if (distance < size) {
-        console.log("test");
-    }
-    
-    //console.log(`${mousePosX} and ${mousePosY}`);
-
     requestAnimationFrame(update);
 }
-update();
 
+update();
