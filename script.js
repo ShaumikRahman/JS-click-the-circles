eccaa5d96087b6dc9dc822346cd3afbc68418241
Shaumik <shaumikrahman123@gmail.com>
2020-08-19 18:47:27 +0100

    remove circle on click

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let mousePosY;
 let collisionCheckX;
 let collisionCheckY;
 let distance;
-let numberOfCircles = 20;
+let numberOfCircles = 100;
 let circleList = [];
 
 document.addEventListener('keydown', function (e) {
@@ -48,22 +48,31 @@ function drawCircle(xPos, yPos) {
     ctx.stroke();
 }
 
-function update() {
-    for (let i = 0; i < numberOfCircles; i++) {
-        drawCircle(circleList[i].x, circleList[i].y)
+onclick = function (e) {
+    mousePosX = e.clientX;
+    mousePosY = e.clientY - document.getElementById('nav').clientHeight;
 
-        onmousemove = function (e) {
-            mousePosX = e.clientX;
-            mousePosY = e.clientY - document.getElementById('nav').clientHeight;
-        }
-
+    for (let i = numberOfCircles - 1; i >= 0; --i) {
         collisionCheckX = circleList[i].x - mousePosX;
         collisionCheckY = circleList[i].y - mousePosY;
         distance = Math.sqrt(collisionCheckX * collisionCheckX + collisionCheckY * collisionCheckY);
 
         if (distance < size) {
-
+            circleList.splice(i, 1);
+            --numberOfCircles;
+            i = -1;
         }
+    }
+}
+
+function update() {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    for (let i = 0; i < numberOfCircles; i++) {
+        drawCircle(circleList[i].x, circleList[i].y);
+
+        // onmousemove = function (e) {
+        
+        // }
 
         //console.log(`${mousePosX} and ${mousePosY}`);
 
